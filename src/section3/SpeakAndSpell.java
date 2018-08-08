fbe21cc39d2bf27cd3d476197a8d74ec52813f5d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-08 14:53:16 -0700

    Day 3

--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -9,11 +9,13 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+speak("Spell Archaeopteryx"));
 		// 2. Catch the user's answer in a String
-
+String answer = JOptionPane.showInputDialog("Answer below");
 		// 3. If the user spelled the word correctly, speak "correct"
-
+if(answer.equalsIgnoreCase("archaeopteryx")) {
+	
+}
 		// 4. Otherwise say "wrong"
 
 		// 5. repeat the process for other words
--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,11 +6,16 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+String answer = JOptionPane.showInputDialog("do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
+if(answer.equalsIgnoreCase("yes")) {
+	JOptionPane.showMessageDialog(null, "You'll rule the world someday.");
+}
 
 		// 3. Otherwise, wish them good luck washing dishes.
-
+else {
+	JOptionPane.showMessageDialog(null, "Good luck washing dishes.");
+}
 	}
 }
 
